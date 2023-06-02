b7c9425439a16d8f1f6f239c1da6baa1f38ce0d8
AKM Elias <akmelias11@gmail.com>
2023-06-02 14:49:33 +0600

    token updates

--- Settings/XenditSettings.php
+++ Settings/XenditSettings.php
@@ -69,10 +69,12 @@ class XenditSettings extends BasePaymentMethod
         $response = wp_remote_get($githubApi, 
         [
             'headers' => array('Accept' => 'application/json',
-            'authorization' => 'bearer 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')
+            'authorization' => 'bearer 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')
         ]);
 
+        // $response = wp_remote_get($githubApi);      
         $releases = json_decode($response['body']);
+  
         if (isset($releases->documentation_url)) {
             return 'no';
         }
